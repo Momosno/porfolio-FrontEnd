6e9423cbc63f2d13e8f36bdc02d81d65be86f8a3
Momosno <uli.dagos@hotmail.com>
2022-12-07 18:58:23 -0300

    pagina base OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AboutComponent } from './components/about/about.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { EducationComponent } from './components/education/education.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 
 
@@ -21,6 +22,7 @@ import { SkillsComponent } from './components/skills/skills.component';
     ExperienceComponent,
     EducationComponent,
     SkillsComponent,
+    ProyectsComponent,
   ],
   imports: [
     BrowserModule,
