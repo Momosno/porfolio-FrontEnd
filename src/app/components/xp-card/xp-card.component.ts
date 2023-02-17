f2dd8774fcf41fb5051b03f0b2342cf392aa5480
Momosno <uli.dagos@hotmail.com>
2023-02-17 13:58:46 -0300

    modal de mier

--- src/app/components/xp-card/xp-card.component.ts
+++ src/app/components/xp-card/xp-card.component.ts
@@ -25,11 +25,22 @@ export class XpCardComponent implements OnInit {
 
   }
 
+
+    
+  html_template:string="<jw-modal id='modal-2'><h1>Edicion de campo</h1><p>Editar Titulo: <textarea style='width: 100%;' rows='2' type='text' [(ngModel)]='this.titulo'></textarea></p><hr><p>Editar Resumen: <textarea style='width: 100%;'' rows='8' type='text' [(ngModel)]='this.resumen'></textarea></p><button (click)='save_button('modal-2',this.titulo,this.resumen)'>HACER QUE SEA SAVE</button></jw-modal>"
+
   modalidachi(id:string,titulo:any,resumen:any){
     this.titulo=titulo
     this.resumen=resumen
     console.log(this.titulo,this.resumen)
     this.modalService.open(id)
   }
+
+  
+  save_button(id:string,titulo:any,resumen:any){
+    console.log(id,titulo,resumen)
+    this.modalService.close()
+  }
+
 
 }
